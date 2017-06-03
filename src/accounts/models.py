15a909d685da3b94dd938a6589845ac0a7736abc
sda06407 <sda06407@gmail.com>
2017-06-03 10:32:39 -0400

    add follow me feacture

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -4,9 +4,18 @@ from django.db import models
 
 # Create your models here.
 
+class UserProfileManager(models.Manager):
+  def all(self):
+    qs = self.get_queryset().all()
+    #print(dir(self))
+    return  qs
+#  def toggle_follow(self, user, to_toggle_user):
+#   user_profile, create = 
+
+objects = UserProfileManager() #UserProfile.objects.all()
 class UserProfile(models.Model):
-  user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="profile")
-  following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')
+  user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="profile") #user.profile
+  following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by') #user.profile.followed_by
   
   def __str__(self):
-    return (str.following.all().count())
+    return str(self.following.all().count())
--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
+from .models import UserProfile
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+User = get_user_model()
+
+class UserProfileTestCase(TestCase):
+  def setUp(self):
+    self.username = "sss"
+    new_user = User.objects.create(username=self.username)
+  def test_profile_created(self):
+    username = self.username
+    user_profile = UserProfile.objects.filter(user__username=self.username)
+    self.assertTrue(user_profile.exists())
+    self.assertTrue(user_profile.count()==1)
